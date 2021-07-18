d50cff538b1ca434ef572be3313b001b66b7ec2b
hsinhui <shinhuei1@gmail.com>
2021-07-18 16:19:24 +0800

    components to global

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
   <div id="app">
-    <RemoveModal/>
-    <RemoveBackModal/>
-    <CancelModal/>
-    <ErrAlert/>
     <loading :active.sync="isLoading" loader="dots"></loading>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import ErrAlert from '@/components/ErrAlert.vue'
-import RemoveModal from '@/components/RemoveModal.vue'
-import CancelModal from '@/components/front/CancelModal.vue'
-import RemoveBackModal from '@/components/back/RemoveBackModal.vue'
 import { mapGetters } from 'vuex'
 export default {
   name: 'App',
-  components: {
-    ErrAlert,
-    RemoveModal,
-    CancelModal,
-    RemoveBackModal
-  },
   computed: {
     ...mapGetters(['isLoading', 'err', 'confirmCancel', 'productId'])
   }
--- src/main.js
+++ src/main.js
@@ -1,93 +1,81 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-// vue-axios 套件
-import axios from 'axios';
-import VueAxios from 'vue-axios';
-// Swiper
-import VueAwesomeSwiper from 'vue-awesome-swiper';
-import 'swiper/swiper-bundle.css';
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/swiper-bundle.css'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import 'bootstrap'
 
-// Alert套件
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
-// loading套件
-import Loading from 'vue-loading-overlay';
-import 'vue-loading-overlay/dist/vue-loading.css';
-// AOS套件
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-// bootstrap 的js
-import 'bootstrap'; // 官方文件說明寫這樣就可載入,https://getbootstrap.com/docs/4.5/getting-started/webpack/
-
-// 表單驗證套件
 import {
   ValidationObserver, // input驗證元件
   ValidationProvider, // 整體<form>驗證元件
   extend, // 擴充功能
   localize, // 語系設定
-  configure, // 設定檔
-} from 'vee-validate';
-import TW from 'vee-validate/dist/locale/zh_TW.json';
-import * as rules from 'vee-validate/dist/rules.js';
+  configure // 設定檔
+} from 'vee-validate'
+import TW from 'vee-validate/dist/locale/zh_TW.json'
+import * as rules from 'vee-validate/dist/rules.js'
 
-// 自行撰寫的程式
-import App from './App.vue';
-import router from './router';
-import './bus';
-import currencyFilter from './filters/currency.js';
-import store from './store';
+import App from './App.vue'
+import router from './router'
+import './bus'
+import currencyFilter from './filters/currency.js'
+import store from './store'
 
-Vue.config.productionTip = false;
-Vue.use(VueAxios, axios); // 套件使用use
-const options = {
-  confirmButtonColor: '#0077b6',
-};
-Vue.use(VueSweetalert2, options);
-Vue.use(VueAwesomeSwiper);
-Vue.use(Vuex);
-AOS.init();
-Vue.component('Loading', Loading); // 將此套件作為元件使用
-// 以下為啟用vee-validate套件相關方法
+import Alert from '@/components/Alert.vue'
+import Modal from '@/components/Modal.vue'
+// import CancelModal from '@/components/front/CancelModal.vue'
+// import RemoveBackModal from '@/components/back/RemoveBackModal.vue'
+Vue.component('alert', Alert)
+Vue.component('modal', Modal)
+
+Vue.config.productionTip = false
+Vue.use(VueAxios, axios) // 套件使用use
+
+Vue.use(VueAwesomeSwiper)
+Vue.use(Vuex)
+AOS.init()
+Vue.component('Loading', Loading)
 Object.keys(rules).forEach((rule) => {
-  extend(rule, rules[rule]); // 將規則全部導出至擴充功能內
-});
-localize('zh_TW', TW);
-Vue.component('ValidationObserver', ValidationObserver);
-Vue.component('ValidationProvider', ValidationProvider);
+  extend(rule, rules[rule])
+})
+localize('zh_TW', TW)
+Vue.component('ValidationObserver', ValidationObserver)
+Vue.component('ValidationProvider', ValidationProvider)
 configure({
   classes: {
     valid: 'is-valid', // 針對bs4設定的className
-    invalid: 'is-invalid',
-  },
-});
+    invalid: 'is-invalid'
+  }
+})
 
-Vue.filter('currency', currencyFilter);
+Vue.filter('currency', currencyFilter)
 
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: (h) => h(App)
+}).$mount('#app')
 
-// 切換頁面時做驗證，避免用戶透過網址切換畫面
 router.beforeEach((to, from, next) => {
-  // 若要到達的頁面有meta.requiresAuth為true的話，進行驗證，沒有的話就直接放行
   if (to.meta.requiresAuth) {
-    const api = `${process.env.VUE_APP_APIPATH}/api/user/check`; // 檢查用戶使否持續登入的api
-    // 在router函式內無法直接使用this.$http，要用axios取代
+    const api = `${process.env.VUE_APP_APIPATH}/api/user/check`
     axios.post(api).then((response) => {
-      // 若登入成功就到達下個頁面，失敗則回到login頁面
       if (response.data.success) {
-        next();
+        next()
       } else {
         next({
-          path: '/login',
-        });
+          path: '/login'
+        })
       }
-    });
+    })
   } else {
-    next();
+    next()
   }
-});
+})
