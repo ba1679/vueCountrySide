7e4e6edad91862e6987b71543e929b40c180127c
hsinhui <shinhuei1@gmail.com>
2021-11-22 13:44:38 +0800

    修正 eslint 錯誤

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from 'vue'
+import Vuex from 'vuex'
 // vue-axios 套件
-import axios from 'axios';
-import VueAxios from 'vue-axios';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 // Swiper
-import VueAwesomeSwiper from 'vue-awesome-swiper';
-import 'swiper/swiper-bundle.css';
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/swiper-bundle.css'
 
 // Alert套件
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 // loading套件
-import Loading from 'vue-loading-overlay';
-import 'vue-loading-overlay/dist/vue-loading.css';
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
 // AOS套件
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 // bootstrap 的js
-import 'bootstrap'; // 官方文件說明寫這樣就可載入,https://getbootstrap.com/docs/4.5/getting-started/webpack/
+import 'bootstrap' // 官方文件說明寫這樣就可載入,https://getbootstrap.com/docs/4.5/getting-started/webpack/
 
 // 表單驗證套件
 import {
@@ -25,69 +25,69 @@ import {
   ValidationProvider, // 整體<form>驗證元件
   extend, // 擴充功能
   localize, // 語系設定
-  configure, // 設定檔
-} from 'vee-validate';
-import TW from 'vee-validate/dist/locale/zh_TW.json';
-import * as rules from 'vee-validate/dist/rules.js';
+  configure // 設定檔
+} from 'vee-validate'
+import TW from 'vee-validate/dist/locale/zh_TW.json'
+import * as rules from 'vee-validate/dist/rules.js'
 
 // 自行撰寫的程式
-import App from './App.vue';
-import router from './router';
-import './bus';
-import currencyFilter from './filters/currency.js';
-import store from './store';
+import App from './App.vue'
+import router from './router'
+import './bus'
+import currencyFilter from './filters/currency.js'
+import store from './store'
 
-Vue.config.productionTip = false;
-Vue.use(VueAxios, axios); // 套件使用use
+Vue.config.productionTip = false
+Vue.use(VueAxios, axios) // 套件使用use
 const options = {
-  confirmButtonColor: '#0077b6',
-};
-Vue.use(VueSweetalert2, options);
-Vue.use(VueAwesomeSwiper);
-Vue.use(Vuex);
-AOS.init();
-Vue.component('Loading', Loading); // 將此套件作為元件使用
+  confirmButtonColor: '#0077b6'
+}
+Vue.use(VueSweetalert2, options)
+Vue.use(VueAwesomeSwiper)
+Vue.use(Vuex)
+AOS.init()
+Vue.component('Loading', Loading) // 將此套件作為元件使用
 // 以下為啟用vee-validate套件相關方法
 Object.keys(rules).forEach((rule) => {
-  extend(rule, rules[rule]); // 將規則全部導出至擴充功能內
-});
-localize('zh_TW', TW);
-Vue.component('ValidationObserver', ValidationObserver);
-Vue.component('ValidationProvider', ValidationProvider);
+  extend(rule, rules[rule]) // 將規則全部導出至擴充功能內
+})
+localize('zh_TW', TW)
+Vue.component('ValidationObserver', ValidationObserver)
+Vue.component('ValidationProvider', ValidationProvider)
 configure({
   classes: {
     valid: 'is-valid', // 針對bs4設定的className
-    invalid: 'is-invalid',
-  },
-});
+    invalid: 'is-invalid'
+  }
+})
 
-Vue.filter('currency', currencyFilter);
+Vue.filter('currency', currencyFilter)
 
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: (h) => h(App)
+}).$mount('#app')
 
 // 切換頁面時做驗證，避免用戶透過網址切換畫面
 router.beforeEach((to, from, next) => {
   // 若要到達的頁面有meta.requiresAuth為true的話，進行驗證，沒有的話就直接放行
   if (to.meta.requiresAuth) {
-    const api = `${process.env.VUE_APP_APIPATH}/api/user/check`; // 檢查用戶使否持續登入的api
+    const api = `${process.env.VUE_APP_APIPATH}/api/user/check` // 檢查用戶使否持續登入的api
     // 在router函式內無法直接使用this.$http，要用axios取代
     axios.post(api).then((response) => {
       // 若登入成功就到達下個頁面，失敗則回到login頁面
       if (response.data.success) {
-        next();
+        next()
       } else {
         next({
-          path: '/login',
-        });
+          path: '/login'
+        })
       }
-    });
+    })
   } else {
-    next();
+    next()
   }
-});
+})
